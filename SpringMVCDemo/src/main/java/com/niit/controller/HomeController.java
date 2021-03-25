d89f9188f45f827f92f2043baedc143f4325ff37
VenkateshBhatOP <74126223+VenkateshBhatOP@users.noreply.github.com>
2021-03-25 17:42:49 +0530

    Add files via upload

---
+++ SpringMVCDemo/src/main/java/com/niit/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.niit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(value = { "/","/home"})
+	public String homePage()
+	{
+		return "index";
+	}
+	
+	@GetMapping("/about")
+	public String aboutUsPage()
+	{
+		return "aboutUs";
+	}
+}
---
+++ SpringMVCDemo/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>HomePage</title>
+</head>
+<body>
+<h2>Hello World!</h2>
+<h3>Welcome to my first Spring MVC Project</h3>
+<a href="about">AboutUS</a>
+</body>
+</html>
